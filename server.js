87238c08eb2d6443a763b0c09e030ac5ff07cefe
Sparramore <steven.parramore@gmail.com>
2018-09-20 17:55:07 -0400

    added PORT

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ db.on("error", function(error) {
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
